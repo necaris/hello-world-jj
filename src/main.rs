12e60287b3a6f40d7fb0004ef145119e0b176438
Rami Chowdhury <rami.chowdhury@gmail.com>
2024-10-05 17:34:22 -0400

    Create hi, bye functions

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@
 /// This is the best implementation of this program to ever exist.
 
 fn main() {
+    print_hi();
+    print_bye();
+}
+
+fn print_hi() {
     println!("Hello, world!");
+}
+
+fn print_bye() {
+    println!("Goodbye.");
 }
